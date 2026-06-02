f06bc402c30103511897c9370c96111a4433e9a1
runner <runner@hmpps-github-actions-runner-796f44cffc-9z7zq>
2026-06-02 15:49:41 +0000

    Rename project

--- src/main/kotlin/uk/gov/justice/digital/hmpps/imsaiapi/ImsAiApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/imsaiapi/ImsAiApi.kt
@@ -1,11 +1,11 @@
-package uk.gov.justice.digital.hmpps.templatepackagename
+package uk.gov.justice.digital.hmpps.imsaiapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HmppsTemplateKotlin
+class ImsAiApi
 
 fun main(args: Array<String>) {
-  runApplication<HmppsTemplateKotlin>(*args)
+  runApplication<ImsAiApi>(*args)
 }
